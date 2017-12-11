6dbbea4ed04a56a267453adf47673537c317d3da
Tomoaki NAKANISHI <e175714@ie.u-ryukyu.ac.jp>
2017-12-11 12:50:57 +0900

    コミット

---
+++ IdeaProjects/Report3/report5/src/main/java/jp/ac/uryukyu/ie/e175714/Enemy.java
@@ -0,0 +1,7 @@
+package jp.ac.uryukyu.ie.e175714;
+
+public class Enemy extends LivingThing{
+    public Enemy (String Ename,int Ehp,int Eattack) {
+        super(Ename,Ehp,Eattack);
+    }
+}
---
+++ IdeaProjects/Report3/report5/src/main/java/jp/ac/uryukyu/ie/e175714/Hero.java
@@ -0,0 +1,18 @@
+package jp.ac.uryukyu.ie.e175714;
+
+public class Hero extends LivingThing{
+    public Hero (String Hname,int Hhp,int Hattack) {
+        super(Hname,Hhp,Hattack);
+    }
+
+
+
+    public void wounded(int damage) {
+
+        sethitpoint(gethitpoint() - damage);
+        if (gethitpoint() < 0) {
+            setdead(true);
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getname());
+        }
+    }
+}
