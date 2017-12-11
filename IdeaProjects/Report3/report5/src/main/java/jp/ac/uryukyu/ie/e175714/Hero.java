4d4d3bccd0c6ccc1cde43db8e36a2477b26e7620
Tomoaki NAKANISHI <e175714@ie.u-ryukyu.ac.jp>
2017-12-11 20:00:56 +0900

    コミット3

--- IdeaProjects/Report3/report5/src/main/java/jp/ac/uryukyu/ie/e175714/Hero.java
+++ IdeaProjects/Report3/report5/src/main/java/jp/ac/uryukyu/ie/e175714/Hero.java
@@ -1,25 +1,25 @@
 package jp.ac.uryukyu.ie.e175714;
-
-public class Hero extends LivingThing{
-    public Hero (String Hname,int Hhp,int Hattack) {
-        super(Hname,Hhp,Hattack);
+public class Hero extends LivingThing {
+    public Hero(String Hname, int Hhp, int Hattack) {
+        super(Hname, Hhp, Hattack);
     }
+
     @Override
     public void attack(LivingThing opponent) {
         int damage = (int) (Math.random() * getattack());
-        int random = (int)(Math.random()*100);
-        if (random<40) {
-            if (damage == 0) {
-                System.out.printf("%sの攻撃！会心の一撃！！,,,だが、%sは攻撃を回避した！\n", getname(), opponent.getname(), damage);
-            }else{
-            System.out.printf("%sの攻撃！会心の一撃！！%sに%dのダメージを与えた！！\n", getname(), opponent.getname(), damage*2);
-            opponent.wounded(damage * 2);
-            }
-        }else{
-            if (getdead() == false) {
+        int random = (int) (Math.random() * 100);
+        if (getdead() == false) {
+            if (random < 40) {
+                if (damage == 0) {
+                    System.out.printf("%sの攻撃！会心の一撃！！,,,だが、%sは攻撃を回避した！\n", getname(), opponent.getname(), damage);
+                } else {
+                    System.out.printf("%sの攻撃！会心の一撃！！%sに%dのダメージを与えた！！\n", getname(), opponent.getname(), damage * 2);
+                    opponent.wounded(damage * 2);
+                }
+            } else {
                 if (damage == 0) {
                     System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getname(), opponent.getname(), damage);
-                }else{
+                } else {
                     System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getname(), opponent.getname(), damage);
                     opponent.wounded(damage);
                 }
@@ -27,8 +27,8 @@ public class Hero extends LivingThing{
         }
     }
 
-    public void wounded(int damage) {
 
+    public void wounded(int damage) {
         sethitpoint(gethitpoint() - damage);
         if (gethitpoint() <= 0) {
             setdead(true);
